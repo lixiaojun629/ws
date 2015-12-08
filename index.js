034e31ae03d969cb1fabc948d1e416a124ddc2cc
lynn.wang <lynn.wang@ucloud.cn>
2015-12-08 12:20:25 +0800

    添加redis 添加config文件 添加index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by L on 2015/12/8.
+ */
+var path = require('path');
+var express = require('express');
+var app = express();
+
+//创建http，ws链接
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//获取config 配置文件
+var config = GLOBAL.config = require("./config.json")[app.get("env")];
+
+//创建redis 客户端
+var redis = require('redis');
+var redis_client = redis.createClient(config.redis_store.port, config.redis_store.host);
+
+//设置环境变量
+app.set('port', config.app_port);
+
+app.get('/', function(req, res){
+    res.send('<h1>Welcome WebSocket Server</h1>');
+});
+
+http.listen(app.get('port'), function(){
+    console.log('listening on *:3000');
+});
