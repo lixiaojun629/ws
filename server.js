ff04e42f4920f6faa6ed03f5a980fd9d741c08b9
lynn.wang <lynn.wang@ucloud.cn>
2015-12-15 01:05:16 +0800

    缺少认证模块

--- server.js
+++ server.js
@@ -11,17 +11,20 @@ var io = require('socket.io')(http);
 
 //获取config 配置文件
 var config = GLOBAL.config = require("./config.json")[app.get("env")];
+var appId = GLOBAL.appId = process.argv[2] || 0;
 
-//创建redis 客户端
-var redis = require('redis');
-var redis_client = redis.createClient(config.redis_store.port, config.redis_store.host);
+var socket = require('./lib/socket.js');
 
 //设置环境变量
 app.set('port', config.app_port);
-
 app.get('/', function(req, res){
     res.send('<h1>Welcome WebSocket Server</h1>');
 });
+
+io.on('connection',function(socket){
+    console.log('SocketIO connection success'+socket.id+":connection "+appId);
+});
+io.use(socket);
 
 var server = http.listen(app.get('port'), function(){
     console.log('WebSocket server listening on port :' + server.address().port);
