adf81d07d3e2fc6d5781a97a478127671711f1c9
rilke <lixiaojun@ucloud.cn>
2016-02-19 15:06:28 +0800

    dev

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ var app = express();
 
 //获取config 配置文件
 var config = GLOBAL.config = require("./config.json")[app.get("env")];
-console.log(config);
 var appId = GLOBAL.appId = process.argv[2] || 0;
 var api_domain = GLOBAL.API_PATH = config.api_domain;
 //创建http，ws链接
@@ -38,12 +37,12 @@ io.use(auth);
  * ws连接断开,在用户的ws连接Set中删除此连接id,若Set为空,则删除Set
  */
 io.on('connection', function (socket) {
-	var email = socket.userEmail;
+	var email = socket.UserEmail;
 	socketLogger.info(email + ' : socket' + socket.id + 'socketid : connection success');
 	userConnDao.save(email ,socket.id);
 
 	//新连接建立,从 持久消息缓存(存储在redis)中取出需要发给此用户的所有消息,发送到客户端
-	messageDao.getUserMessages(socket.userEmail).then(function(userMessages){
+	messageDao.getUserMessages(socket.UserEmail).then(function(userMessages){
 		userMessages.forEach(function(message){
 			socket.emit('message',message);
 		})
@@ -51,27 +50,62 @@ io.on('connection', function (socket) {
 
 	socket.on('disconnect', function () {
 		socketLogger.info(socket.id + ":disconnect " + appId);
-		userConnDao.remove(socket.userEmail,socket.id);
+		userConnDao.remove(socket.UserEmail,socket.id);
 	});
 });
 
 
 server.listen(app.get('port'), function () {
-	console.log('WebSocket server listening on port :' + server.address().port);
+	socketLogger.info('WebSocket server listening on port :' + server.address().port);
 	subscribe('test_channel').then(function(client){
-        console.log('subscribe test_channel');
+        socketLogger.info('subscribe test_channel');
 		client.on('message',function(channel,messageStr){
-            console.log(messageStr);
+            socketLogger.info('receive message:',messageStr);
 			message = JSON.parse(messageStr);
-			if(message.userEmailList === 'ALL'){
-
-			}else{
-				message.userEmailList.forEach(function(email){
-					userConnDao.getAllUserConn(email).then(function(socketIds){
-
-					})
-				})
-			}
+            var nowTimeStamp = (Date.now()/1000).toFixed(0);
+            if(message.BeginTime > nowTimeStamp){
+                setTimeout(onMessage, (message.BeginTime - nowTimeStamp) * 1000);
+            }else {
+                onMessage(message);
+            }
 		})
 	})
 });
+
+function onMessage (message){
+    if(message.ExpireTime && message.ExpireTime > Date.now()/1000){
+        messageDao.save(JSON.stringify(message));
+    }
+    sendMessage(message);
+}
+
+function sendMessage(message){
+    var messageStr = JSON.stringify(message.Body);
+    var sockets = [];
+
+    if(message.SessionId){
+        sockets = io.sockets.sockets.filter(function(socket){
+            return message.SessionId === socket.id;
+        })
+    }
+
+    if(message.UserEmail){
+        sockets = io.sockets.sockets.filter(function(socket){
+            return socket.UserEmail === socket.UserEmail;
+        });
+    }
+
+    if(message.CompanyId){
+        sockets = io.sockets.sockets.filter(function(socket){
+            return socket.CompanyId === socket.CompanyId;
+        });
+    }
+
+    if(message.AllUser === true){
+        io.emit('message',messageStr);
+    }
+
+    sockets.forEach(function(socket){
+        socket.emit('message',messageStr);
+    });
+}
