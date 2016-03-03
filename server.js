61bee67299891737644d517ae96ca87ca90f4bce
rilke <lixiaojun@ucloud.cn>
2016-03-03 21:53:15 +0800

    dev

--- server.js
+++ server.js
@@ -13,62 +13,65 @@ var auth = require('./lib/auth');
 var util = require('./lib/util');
 var cache = require('./lib/cache');
 var dao = require('./lib/dao');
+var user = require('./lib/user');
 var Const = require('./lib/const');
 
 var app = express();
 var server = http.createServer(app);
-var wss = new WebSocketServer({server: server});
+var wss = new WebSocketServer({server: server,perMessageDeflate:false});
 var messageDao = dao.messageDao;
 
 config = config[app.get('env')];
 app.set('port', config.app_port);
 
 server.listen(app.get('port'), function () {
-	logger.info('WebSocket Server listening on port :' + server.address().port);
+    logger.info('WebSocket Server listening on port :' + server.address().port);
+    util.subscribe(Const.CHANNEL).then(function (client) {
+        client.on('message', function (channel, messageStr) {
+            logger.info('receive message:', messageStr);
+            message = JSON.parse(messageStr);
+            onMessage(message);
+        })
+    });
 });
 
 wss.on('connection', function (socket) {
 	auth.verify(socket)
+        .then(user.fetchProjectList)
 		.then(onConnect)
-		.fail(function (error) {
-			socket.send(error);
-			socket.close();
-		});
-});
-
-util.subscribe(Const.CHANNEL).then(function (client) {
-	client.on('message', function (channel, messageStr) {
-		logger.info('receive message:', messageStr);
-		message = JSON.parse(messageStr);
-		onMessage(message);
-	})
+        .fail(function(error){
+            logger.error(error);
+        })
 });
 
 function onConnect(socket) {
 	socket.id = uuid.v4();
-	logger.info("UserId:" + socket.UserId + "##" + "SessionId: " + socket.id + '  connected');
+	logger.info("UserId:" + socket.UserId + "##" + "SessionId:" + socket.id + '  connected');
 	//把SocketId发送到客户端，作为SessionId
+
+	socket.send(socket.id);
+	cache.save(socket);
 
 	socket.on('open', function open() {
 		console.log("opened");
-		socket.send(socket.id);
-		cache.save(socket);
 	});
 
 	socket.on('close', function close() {
-		logger.info("UserId:" + socket.UserId + "##" + "SessionId: " + socket.id + '  closed');
+		logger.info("UserId:" + socket.UserId + "##" + "SessionId:" + socket.id + '  closed');
 		cache.remove(socket);
 	});
 
 	socket.on('message', function message(data, flags) {
+        socket.send('pong');
 		console.log("receive message", arguments);
+        console.log("send pong");
 	});
 
 	//新连接建立,从 持久消息缓存(存储在redis)中取出需要发给此用户的所有消息,发送到客户端
 	messageDao.getConnMessages(socket)
 		.then(function (userMessages) {
 			userMessages.forEach(function (message) {
-				socket.emit('message', message.Body);
+				socket.send(JSON.stringify(message.Body));
 			})
 		}, function (error) {
 			logger.error(error);
@@ -97,6 +100,6 @@ function sendMessage(message) {
 		sockets = cache.company[message.CompanyId];
 	}
 	sockets && sockets.forEach(function (socket) {
-		socket.send("message", messageStr);
+		socket.send(messageStr);
 	});
 }
