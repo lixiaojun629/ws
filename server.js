1ee74c21280ce6ae0a382c00ac0e4a4cdd80a224
lixiaojun <lixiaojun@ucloud.cn>
2016-03-03 18:10:59 +0800

    dev

--- server.js
+++ server.js
@@ -7,20 +7,21 @@ var http = require('http');
 var WebSocketServer = require('ws').Server;
 
 var config = require("./config.json");
+var app = express();
+config = config[app.get['env']];
+GLOBAL.config = config;
+
 var logger = require('./lib/logger').logger("socket");
 var auth = require('./lib/auth');
 var util = require('./lib/util');
-var connCache = require('./lib/conn_cache');
+var cache = require('./lib/cache');
 var dao = require('./lib/dao');
 var Const = require('./lib/const');
 
-var app = express();
 var server = http.createServer(app);
 var wss = new WebSocketServer({server: server});
 var messageDao = dao.messageDao;
 
-config = config[app.get['env']];
-GLOBAL.config = config;
 
 app.set('port', config.app_port);
 
@@ -53,12 +54,12 @@ function onConnect(socket) {
 	socket.on('open', function open() {
 		console.log("opened");
 		socket.send(socket.id);
-		connCache.save(socket);
+		cache.save(socket);
 	});
 
 	socket.on('close', function close() {
 		logger.info("UserId:" + socket.UserId + "##" + "SessionId: " + socket.id + '  closed');
-		connCache.remove(socket);
+		cache.remove(socket);
 	});
 
 	socket.on('message', function message(data, flags) {
@@ -89,13 +90,13 @@ function sendMessage(message) {
 	if (message.AllUser === true) {
 		sockets = wss.clients;
 	} else if (message.SessionId) {
-		sockets = [connCache.session[message.SessionId]];
+		sockets = [cache.session[message.SessionId]];
 	} else if (message.UserId) {
-		sockets = connCache.user[message.UserId];
+		sockets = cache.user[message.UserId];
 	} else if (message.ProjectId) {
-		sockets = connCache.project[message.ProjectId];
+		sockets = cache.project[message.ProjectId];
 	} else if (message.CompanyId) {
-		sockets = connCache.company[message.CompanyId];
+		sockets = cache.company[message.CompanyId];
 	}
 	sockets && sockets.forEach(function (socket) {
 		socket.send("message", messageStr);
